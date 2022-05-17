adf7d2da5236410b101543ac3239cebe68adfa9a
ziko0505072 <ziko0505072@gmail.com>
2022-05-17 22:23:41 +0500

    version 17.05.2022

--- Golos_Navoiy.sql
+++ Golos_Navoiy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 13 2022 г., 14:31
+-- Время создания: Май 17 2022 г., 20:23
 -- Версия сервера: 5.6.37
 -- Версия PHP: 5.5.38
 
@@ -31,8 +31,30 @@ SET time_zone = "+00:00";
 CREATE TABLE `application` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `application` varchar(700) NOT NULL
+  `application` varchar(700) NOT NULL,
+  `answer_aplicate` varchar(225) NOT NULL DEFAULT 'None',
+  `answer_doc` varchar(55) NOT NULL DEFAULT 'None',
+  `date` varchar(12) NOT NULL,
+  `end_date` date NOT NULL,
+  `app_type` varchar(45) NOT NULL DEFAULT 'new'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `application`
+--
+
+INSERT INTO `application` (`id`, `user_id`, `application`, `answer_aplicate`, `answer_doc`, `date`, `end_date`, `app_type`) VALUES
+(1, 1078736196, 'iltimos qiynagan savolingizni bravering, yaqin orada biz javob berishga harakat qilamiz', 'sizga yaxshi sharoit qilinadi', 'None', '12-04-2022', '0000-00-00', 'new'),
+(2, 1078736196, 'siz registraciyadan alla qachon utgan siz', 'sizga yaxshi sharoit qilinadi', 'None', '14-98-2478', '0000-00-00', 'new'),
+(3, 1078736196, 'TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 iltimos qiynagan savolingizni bravering, yaqin orada biz javob berishga harakat qilamiz', 'sizga yaxshi sharoit qilinadi', 'None', '78-96-5412', '0000-00-00', 'new'),
+(4, 1078736196, 'sizning arizangiz qabul qilindi, va yaqin orada biz javob berishga harakat qilamiz//////////////////', 'sizga yaxshi sharoit qilinadi', 'None', '36-98-25', '0000-00-00', 'new'),
+(5, 1078736196, 'datetimedatetimedatetimedatetimedatetimedatetimedatetimedatetime', 'sizga yaxshi sharoit qilinadi', 'None', '2022-04-28', '0000-00-00', 'new'),
+(6, 1078736196, 'datetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimedatetimeasdasdasssssssssssssssss', 'sizga yaxshi sharoit qilinadi', 'None', '2022-04-28', '0000-00-00', 'new'),
+(7, 1078736196, 'asdasfhghjoikljnrifjdksgfhnueklhdfjmxadsfafasdasd', 'sizga yaxshi sharoit qilinadi', 'None', '2022-04-28', '0000-00-00', 'new'),
+(8, 315375144, 'iltimos qiynagan savolingizni bravering, yaqin orada biz javob berishga harakat qilamiz//////////////////////////////////////////////////////////////////', 'sizga yaxshi sharoit qilinadi', 'None', '2022-4-30', '0000-00-00', 'new'),
+(9, 315375144, 'iltimos qiynagan savolingizni bravering, yaqin orada biz javob berishga harakat qilamiz//////////////////////////////////////////////////////////////////asdasdasdasdasdasd', 'sizga yaxshi sharoit qilinadi', 'None', '2022-4-30', '0000-00-00', 'new'),
+(10, 315375144, 'asldgaslhgdalsfglasfgladhgfladshgflsdhagflsdhfgjhsgfsldhfgsldhfgslhfgslhdfgslhdfgslfgsldhfgsdhjkgfsjhkdgfsjdhfgsjhdfglshdfglshdgflskdjhlkjhgflskjhdflskdhgfashalskdbxcnbvlajgflkdfbnlhfdlkd', 'sizga yaxshi sharoit qilinadi', 'None', '2022-4-30', '0000-00-00', 'new'),
+(11, 315375144, 'Qachon yollar yaxshilanadi', 'None', 'None', '2022-5-17', '0000-00-00', 'new');
 
 -- --------------------------------------------------------
 
@@ -45,15 +67,17 @@ CREATE TABLE `users` (
   `fio` varchar(40) NOT NULL,
   `User_id` varchar(20) NOT NULL,
   `Phone` varchar(15) NOT NULL,
-  `Adres` varchar(50) NOT NULL
+  `Adres` varchar(50) NOT NULL,
+  `types` varchar(20) NOT NULL DEFAULT 'client'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `fio`, `User_id`, `Phone`, `Adres`) VALUES
-(3, 'Aziz Nazarov', '1078736196', '+998907318801', 'uzbekistan 2-10');
+INSERT INTO `users` (`id`, `fio`, `User_id`, `Phone`, `Adres`, `types`) VALUES
+(3, 'Aziz Nazarov', '1078736196', '+998907318801', 'uzbekistan 2-10', 'operator'),
+(4, 'Xilola Xodjaeva', '315375144', '+998914157530', 'uzbekisrtan 2-10', 'user');
 
 --
 -- Индексы сохранённых таблиц
@@ -79,12 +103,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `application`
 --
 ALTER TABLE `application`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
